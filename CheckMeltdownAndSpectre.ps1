90b72f493e557054294eae1e75c3e80dc80236be
Robert Rozas Navarro <robert.rozas.n@outlook.com>
2018-01-08 11:44:05 -0300

    Script in place

---
+++ CheckMeltdownAndSpectre.ps1
@@ -0,0 +1,7 @@
+﻿#Run Powershell as Admin and Install the Following Module
+#Install-Module SpeculationControl
+
+#Then run this script
+Import-Module SpeculationControl
+
+Get-SpeculationControlSettings
